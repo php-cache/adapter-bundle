c362ad6543c39d3ceac3aeee3dad6b9be554ddb3
Aaron Scherer <aequasi@gmail.com>
2016-02-04 14:41:55 -0800

    Adding chain factory, and allowing options to just be an indexed array of service ids

---
+++ src/Factory/ChainFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of php-cache organization.
+ *
+ * (c) 2015-2015 Aaron Scherer <aequasi@gmail.com>, Tobias Nyholm <tobias.nyholm@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Cache\AdapterBundle\Factory;
+
+use Cache\Adapter\Chain\CachePoolChain;
+
+/**
+ * @author Aaron Scherer <aequasi@gmail.com>
+ */
+class ChainFactory extends AbstractAdapterFactory
+{
+    protected static $dependencies = [
+        ['requiredClass' => 'Cache\Adapter\Chain\CachePoolChain', 'packageName' => 'cache/chain-adapter'],
+    ];
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAdapter(array $config)
+    {
+        return new CachePoolChain($config);
+    }
+}
