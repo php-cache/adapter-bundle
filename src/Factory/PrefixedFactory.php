a9dfd71551214102a8a08149180db452c4cb481c
Iain Mckay <me@iainmckay.co.uk>
2017-02-24 14:50:42 +0100

    Add support for prefixed cache

---
+++ src/Factory/PrefixedFactory.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * This file is part of php-cache organization.
+ *
+ * (c) 2015-2015 Aaron Scherer <aequasi@gmail.com>, Tobias Nyholm <tobias.nyholm@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Cache\AdapterBundle\Factory;
+
+use Cache\Prefixed\PrefixedCachePool;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PrefixedFactory extends AbstractAdapterFactory
+{
+    protected static $dependencies = [
+        ['requiredClass' => 'Cache\Prefixed\PrefixedCachePool', 'packageName' => 'cache/prefixed-cache'],
+    ];
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAdapter(array $config)
+    {
+        return new PrefixedCachePool($config['service'], $config['prefix']);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected static function configureOptionResolver(OptionsResolver $resolver)
+    {
+        parent::configureOptionResolver($resolver);
+
+        $resolver->setRequired(['prefix', 'service']);
+        $resolver->setAllowedTypes('service', ['object']);
+        $resolver->setAllowedTypes('prefix', ['string']);
+    }
+}
