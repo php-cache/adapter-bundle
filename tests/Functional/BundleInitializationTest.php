7a23f83eebd8143f3ed403a6111efad0ad28bb2d
Tobias Nyholm <tobias.nyholm@gmail.com>
2017-03-28 13:34:12 +0200

    Added more tests (#55)
    * Added more tests
    
    * cs
    
    * Added more tests and travis config
    
    * Bugfixes
    
    * Separating tests
    
    * syntax error
    
    * style fix
    
    * Removed redis installation
    
    * Added redis server service
    
    * Do not add extra config under hhvm
    
    * Require doctrine 1.6
    
    * Added memcache server
    
    * Do not update composer
    
    * Minor
    
    * Bugfix

--- tests/Functional/BundleInitializationTest.php
+++ tests/Functional/BundleInitializationTest.php
@@ -11,10 +11,80 @@
 
 namespace Cache\AdapterBundle\Tests\Functional;
 
-class BundleInitializationTest extends BaseTestCase
+use Cache\Adapter\Apc\ApcCachePool;
+use Cache\Adapter\Apcu\ApcuCachePool;
+use Cache\Adapter\Chain\CachePoolChain;
+use Cache\Adapter\Doctrine\DoctrineCachePool;
+use Cache\Adapter\Memcache\MemcacheCachePool;
+use Cache\Adapter\Memcached\MemcachedCachePool;
+use Cache\Adapter\PHPArray\ArrayCachePool;
+use Cache\Adapter\Predis\PredisCachePool;
+use Cache\Adapter\Redis\RedisCachePool;
+use Cache\Adapter\Void\VoidCachePool;
+use Cache\AdapterBundle\CacheAdapterBundle;
+use Nyholm\BundleTest\BaseBundleTestCase;
+
+class BundleInitializationTest extends BaseBundleTestCase
 {
-    public function testRegisterBundle()
+    protected function getBundleClass()
     {
-        static::createClient();
+        return CacheAdapterBundle::class;
+    }
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $kernel = $this->createKernel();
+        $kernel->addConfigFile(__DIR__.'/config.yml');
+    }
+
+    public function testFactoriesWithWithDefaultConfiguration()
+    {
+        $this->bootKernel();
+        $container = $this->getContainer();
+        $this->assertInstanceOf(ArrayCachePool::class, $container->get('alias.my_adapter'));
+        $this->assertInstanceOf(ApcCachePool::class, $container->get('cache.provider.apc'));
+        $this->assertInstanceOf(ApcuCachePool::class, $container->get('cache.provider.apcu'));
+        $this->assertInstanceOf(ArrayCachePool::class, $container->get('cache.provider.array'));
+        $this->assertInstanceOf(CachePoolChain::class, $container->get('cache.provider.chain'));
+        $this->assertInstanceOf(PredisCachePool::class, $container->get('cache.provider.predis'));
+        $this->assertInstanceOf(VoidCachePool::class, $container->get('cache.provider.void'));
+
+        $this->assertInstanceOf(DoctrineCachePool::class, $container->get('cache.provider.doctrine_filesystem'));
+        $this->assertInstanceOf(DoctrineCachePool::class, $container->get('cache.provider.doctrine_predis'));
+    }
+
+    public function testMemcachedWithWithDefaultConfiguration()
+    {
+        if (!class_exists('Memcached')) {
+            $this->markTestSkipped('Skipping since Memcached is not installed.');
+        }
+        $this->bootKernel();
+        $container = $this->getContainer();
+        $this->assertInstanceOf(MemcachedCachePool::class, $container->get('cache.provider.memcached'));
+        $this->assertInstanceOf(DoctrineCachePool::class, $container->get('cache.provider.doctrine_memcached'));
+    }
+
+    public function testMemcacheWithWithDefaultConfiguration()
+    {
+        if (!class_exists('Memcache')) {
+            $this->markTestSkipped('Skipping since Memcache is not installed.');
+        }
+        $this->bootKernel();
+        $container = $this->getContainer();
+        $this->assertInstanceOf(MemcacheCachePool::class, $container->get('cache.provider.memcache'));
+        $this->assertInstanceOf(DoctrineCachePool::class, $container->get('cache.provider.doctrine_memcache'));
+    }
+
+    public function testRedisWithWithDefaultConfiguration()
+    {
+        if (!class_exists('Redis')) {
+            $this->markTestSkipped('Skipping since Memcache is not installed.');
+        }
+
+        $this->bootKernel();
+        $container = $this->getContainer();
+        $this->assertInstanceOf(RedisCachePool::class, $container->get('cache.provider.redis'));
+        $this->assertInstanceOf(DoctrineCachePool::class, $container->get('cache.provider.doctrine_redis'));
     }
 }
