bce99fc5c5697b441b8e7a116c137288f8183999
Tobias Nyholm <tobias.nyholm@gmail.com>
2017-03-28 12:37:19 +0200

    Added more tests

--- tests/Functional/BundleInitializationTest.php
+++ tests/Functional/BundleInitializationTest.php
@@ -11,10 +11,60 @@
 
 namespace Cache\AdapterBundle\Tests\Functional;
 
-class BundleInitializationTest extends BaseTestCase
+use Cache\Adapter\Apcu\ApcuCachePool;
+use Cache\Adapter\Chain\CachePoolChain;
+use Cache\Adapter\Memcached\MemcachedCachePool;
+use Cache\Adapter\PHPArray\ArrayCachePool;
+use Cache\Adapter\Predis\PredisCachePool;
+use Cache\Adapter\Redis\RedisCachePool;
+use Cache\Adapter\Void\VoidCachePool;
+use Cache\AdapterBundle\CacheAdapterBundle;
+use Cache\AdapterBundle\Factory\ApcFactory;
+use Nyholm\BundleTest\BaseBundleTestCase;
+
+class BundleInitializationTest extends BaseBundleTestCase
 {
-    public function testRegisterBundle()
+    protected function getBundleClass()
     {
-        static::createClient();
+        return CacheAdapterBundle::class;
+    }
+
+    /**
+     * Make sure the bundle can initialize
+     */
+    public function testInitBundle()
+    {
+        // Boot the kernel.
+        $this->bootKernel();
+
+        // Get the container
+        $container = $this->getContainer();
+
+        // Test if you services exists
+        $this->assertTrue($container->has('cache.factory.apc'));
+        $service = $container->get('cache.factory.apc');
+        $this->assertInstanceOf(ApcFactory::class, $service);
+    }
+
+    public function testFactoriesWithWithDefaultConfiguration()
+    {
+        // Create a new Kernel
+        $kernel = $this->createKernel();
+
+        // Add some configuration
+        $kernel->addConfigFile(__DIR__.'/config.yml');
+
+        // Boot the kernel as normal ...
+        $this->bootKernel();
+
+        $container = $this->getContainer();
+        $this->assertInstanceOf(ArrayCachePool::class, $container->get('alias.my_adapter'));
+        $this->assertInstanceOf(ApcuCachePool::class, $container->get('cache.provider.apcu'));
+        $this->assertInstanceOf(ArrayCachePool::class, $container->get('cache.provider.array'));
+        $this->assertInstanceOf(CachePoolChain::class, $container->get('cache.provider.chain'));
+        $this->assertInstanceOf(MemcachedCachePool::class, $container->get('cache.provider.memcached'));
+        $this->assertInstanceOf(PredisCachePool::class, $container->get('cache.provider.predis'));
+        $this->assertInstanceOf(RedisCachePool::class, $container->get('cache.provider.redis'));
+        $this->assertInstanceOf(VoidCachePool::class, $container->get('cache.provider.void'));
     }
 }
