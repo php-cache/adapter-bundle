eaa3684234f76235ecca0fb6ef7c4eab2fe000db
Aaron Scherer <aequasi@gmail.com>
2018-03-20 13:35:51 -0700

    Adding autowiring for providers

--- src/CacheAdapterBundle.php
+++ src/CacheAdapterBundle.php
@@ -11,6 +11,8 @@
 
 namespace Cache\AdapterBundle;
 
+use Cache\AdapterBundle\DependencyInjection\CompilerPass\ServiceAliasCompilerPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 /**
@@ -18,4 +20,13 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  */
 class CacheAdapterBundle extends Bundle
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new ServiceAliasCompilerPass());
+    }
 }
---
+++ src/DependencyInjection/CompilerPass/ServiceAliasCompilerPass.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * This file is part of cache-bundle.
+ *
+ * (c) Aaron Scherer <aequasi@gmail.com>
+ *
+ * This source file is subject to the license that is bundled
+ * with this source code in the file LICENSE
+ */
+
+namespace Cache\AdapterBundle\DependencyInjection\CompilerPass;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+/**
+ * @author Aaron Scherer <aequasi@gmail.com>
+ *
+ * ServiceAliasCompilerPass Class
+ */
+class ServiceAliasCompilerPass implements CompilerPassInterface
+{
+
+    /**
+     * You can modify the container here before it is dumped to PHP code.
+     *
+     * @param ContainerBuilder $container
+     */
+    public function process(ContainerBuilder $container)
+    {
+        $serviceIds = array_keys($container->findTaggedServiceIds('cache.provider'));
+        foreach ($serviceIds as $serviceId) {
+            $cleanName = str_replace('.inner', '', $serviceId);
+            $instance  = $container->get($serviceId);
+            $class     = get_class($instance);
+
+            $container->setAlias($cleanName, $serviceId);
+            $container->setAlias($class, $cleanName);
+        }
+    }
+}
