6220c0b5aeacf89799abce95bfb282a4769a6a6a
Nyholm <tobias.nyholm@gmail.com>
2018-07-08 17:09:30 +0200

    Enable memcache

--- .travis/load_php_extensions.sh
+++ .travis/load_php_extensions.sh
@@ -7,15 +7,14 @@ if [ $(phpenv version-name) = "5.6" ]; then
     # PHP 5.6
     echo "Install APC Adapter & APCu Adapter dependencies"
     yes '' | pecl install -f apcu-4.0.11
+
+    echo "Install redis"
+    yes '' | pecl install -f redis-2.2.8
 else
     # PHP 7.0
     echo "Install APCu Adapter dependencies"
     yes '' | pecl install -f apcu-5.1.8
+
+    echo "Install redis"
+    yes '' | pecl install -f redis-3.0.0
 fi
-
-
-echo "Install memcache(d)"
-echo "extension = memcached.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
-
-echo "Install redis"
-yes '' | pecl install -f redis-3.0.0
