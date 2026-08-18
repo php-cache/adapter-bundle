4c84842f3713a8fff4d9a7f5c978c005d93e8305
Aaron Scherer <aequasi@gmail.com>
2026-08-18 14:19:26 -0500

    style: enforce project headers
    match the configured StyleCI header and enforce it through the local
    fixer.

--- tools/check-coverage.php
+++ tools/check-coverage.php
@@ -2,6 +2,15 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of php-cache organization.
+ *
+ * (c) 2015 Aaron Scherer <aequasi@gmail.com>, Tobias Nyholm <tobias.nyholm@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 $arguments = $_SERVER['argv'] ?? [];
 if (!is_array($arguments) || !array_is_list($arguments) || 3 !== count($arguments) || !is_string($arguments[1]) || !is_string($arguments[2]) || !is_numeric($arguments[2])) {
     fwrite(\STDERR, "Usage: php tools/check-coverage.php <clover.xml> <minimum-percent>\n");
